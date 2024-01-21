1fd4b9b3e424aae8d05fca15d8cdb23a9c14cd02
Kickblip <wyatt.zilker@gmail.com>
2024-01-21 13:23:06 -0700

    navbar

--- components/HomeIntro.tsx
+++ components/HomeIntro.tsx
@@ -11,24 +11,26 @@ const HomeIntro: React.FC<Props> = ({ title, description }) => {
     const logoSrc = "/hapi-logo.png";
 
     return (
-        <div className="flex w-full h-screen">
-            <div className="flex-1 flex items-center justify-center p-8">
-                <div>
-                    <img src={logoSrc} alt="HAPI Lab Logo" className="mb-8 h-40" />
+        <div className="h-screen">
+            <div className="flex w-full h-[95%]">
+                <div className="flex-1 flex items-center justify-center p-8">
+                    <div>
+                        <img src={logoSrc} alt="HAPI Lab Logo" className="mb-8 h-32" />
 
-                    <h2 className="text-5xl font-bold dark:text-[#d4d4d4] text-[#39c9c9c]">{title}</h2>
-                    <p className="mt-4 text-xl dark:text-[#9c9c9c] text-[#505052]">{description}</p>
+                        <h2 className="text-5xl font-bold dark:text-[#d4d4d4] text-[#39c9c9c]">{title}</h2>
+                        <p className="mt-4 text-xl dark:text-[#9c9c9c] text-[#505052]">{description}</p>
 
-                    <Link href="/your-link">
-                        <button className="mt-4 text-blue-500 border-2 border-blue-500 py-2 px-4 rounded hover:bg-blue-600 hover:text-white transition-colors duration-200">
-                            Button Text
-                        </button>
-                    </Link>
+                        {/* <Link href="/your-link">
+                            <button className="mt-8 text-blue-500 border-2 border-blue-500 py-2 px-4 rounded hover:bg-blue-600 hover:text-white transition-colors duration-200">
+                                Button Text
+                            </button>
+                        </Link> */}
 
+                    </div>
                 </div>
-            </div>
-            <div className="flex-1">
-                <img src={imageSrc} alt="Turtlebot" className="w-full h-full object-cover" />
+                <div className="flex-1">
+                    <img src={imageSrc} alt="Turtlebot" className="w-full h-full object-cover" />
+                </div>
             </div>
         </div>
     )
---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="text-center text-lg font-medium space-x-6 my-4">
+        <Link className="hover:text-blue-500 transition-colors duration-200" href="/research">Research</Link>
+        <Link className="hover:text-blue-500 transition-colors duration-200" href="/team">Team</Link>
+        <Link className="hover:text-blue-500 transition-colors duration-200" href="/demos">Demos</Link>
+        <Link className="hover:text-blue-500 transition-colors duration-200" href="/awards">Awards</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
