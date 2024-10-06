62fce863aeb498461d7f89e1892e5991e5aa8a6c
Kickblip <wyatt.zilker@gmail.com>
2024-10-06 11:36:05 -0500

    loading button and hero fix

--- app/contact/ContactForm.tsx
+++ app/contact/ContactForm.tsx
@@ -1,9 +1,19 @@
+"use client"
+
+import { useState } from "react"
+
 export default function ContactForm() {
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
   return (
     <div className="max-w-md mx-auto p-8">
       <h3 className="text-xl text-primary-500 font-bold">INTERESTED IN THE LAB?</h3>
       <h2 className="text-4xl text-gold-500 font-black mb-6">JOIN THE HAPINESS</h2>
-      <form action={`https://formsubmit.co/${process.env.NEXT_PUBLIC_FORMSUBMIT_CONTACT}`} method="POST">
+      <form
+        action={`https://formsubmit.co/${process.env.NEXT_PUBLIC_FORMSUBMIT_CONTACT}`}
+        method="POST"
+        onSubmit={() => setIsSubmitting(true)}
+      >
         <input type="text" name="_honey" className="hidden"></input>
         <input type="hidden" name="_next" value={`${process.env.NEXT_PUBLIC_SITE_URL}/contact/success`}></input>
         <div className="grid grid-cols-1 gap-2">
@@ -49,8 +59,21 @@ export default function ContactForm() {
             <button
               type="submit"
               className="ml-auto py-1.5 px-6 bg-gold-500 text-white text-sm font-semibold shadow rounded focus:outline-none"
+              disabled={isSubmitting}
             >
-              SUBMIT
+              {isSubmitting ? (
+                <svg
+                  className="animate-spin h-5 w-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                  <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"></path>
+                </svg>
+              ) : (
+                "SUBMIT"
+              )}
             </button>
           </div>
         </div>
--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -1,15 +1,15 @@
 export default function Hero() {
   return (
-    <>
-      <div className="relative h-[80vh] w-full overflow-hidden grid grid-cols-1 md:grid-cols-[1fr_auto] bg-primary-800">
-        <div className="text-white text-left p-8 pl-16 flex items-center">
-          <h1 className="text-7xl md:text-7xl font-bold mb-4">Human-Aware Planning & Interaction Lab</h1>
+    <div className="bg-primary-800 pt-16">
+      <div className="relative md:h-[70vh] w-full overflow-hidden grid grid-cols-1 md:grid-cols-[1fr_auto]">
+        <div className="text-white text-left p-8 md:pl-16 flex items-center">
+          <h1 className="text-5xl md:text-7xl font-bold mt-20 mb-20 md:mt-0 md:mb-4">Human-Aware Planning & Interaction Lab</h1>
         </div>
-        <div className="relative h-full w-[52rem] overflow-hidden hidden md:block">
+        <div className="relative h-full w-[50rem] overflow-hidden hidden md:block">
           <img src="/landing/Ram-Background.jpg" alt="Background" className="absolute top-0 left-0 w-full h-full object-cover" />
         </div>
       </div>
       <div className="bg-gold-500 w-full h-4"></div>
-    </>
+    </div>
   )
 }
